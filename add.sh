b96f6d4299dc88dd1885ade5a25bfb1f206f7a13
saiprakash774 <69646766+saiprakash774@users.noreply.github.com>
2020-10-01 20:06:21 +0530

    test

--- add.sh
+++ add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-read -p "Enter first number: " x
-read -p "Enter second number: " y
-z=$(( $x + $y ))
-echo $z
+read -p "Enter first number: " a
+read -p "Enter second number: " b
+c=$(( $a + $b ))
+echo $c
